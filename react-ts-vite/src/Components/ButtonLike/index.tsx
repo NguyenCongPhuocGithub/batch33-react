544987d88b96d8b7b3f8266eefdc371368569ebc
Cong Phuoc <nguyencongphuoccv@gmail.com>
2023-08-03 23:36:51 +0700

    Upload Day05

--- react-ts-vite/src/Components/ButtonLike/index.tsx
+++ react-ts-vite/src/Components/ButtonLike/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import {AiOutlineLike} from 'react-icons/ai'
-const ButtonLike = () => {
-    const [Liked, setLiked] = React.useState(false);
-    const LikeState = () => {
-        setLiked(!Liked);
-    }
-    return (
-        <>
-            <button onClick={LikeState} style={{color : Liked && Liked === true ? "blue" : ''}}>
-                    <AiOutlineLike/>
-            </button>
-        </>
-    )
-}
+// import React from 'react'
+// import {AiOutlineLike} from 'react-icons/ai'
+// const ButtonLike = () => {
+//     const [Liked, setLiked] = React.useState(false);
+//     const LikeState = () => {
+//         setLiked(!Liked);
+//     }
+//     return (
+//         <>
+//             <button onClick={LikeState} style={{color : Liked && Liked === true ? "blue" : ''}}>
+//                     <AiOutlineLike/>
+//             </button>
+//         </>
+//     )
+// }
 
-export default ButtonLike
+// export default ButtonLike
